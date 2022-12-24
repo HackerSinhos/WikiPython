0b17d2a308118ec677ba8d5c30e0c093ff318b8b
HackerSinhos <nunseinada01@gmail.com>
2022-12-23 21:27:16 -0300

    Added error if not finding the word on Wikipedia.

--- Wikipython.py
+++ Wikipython.py
@@ -1,7 +1,14 @@
 import ctypes
 import wikipedia as wiki
-word = input("who is you searching? ")
-info = wiki.summary(word)
-ctypes.windll.user32.MessageBoxW(
-    0, info[:400], word, 0
-)
+
+word = input("Who are you searching? ")
+
+try:
+    info = wiki.summary(word)
+    ctypes.windll.user32.MessageBoxW(
+        0, info[:400], word, 0
+    )
+except wiki.exceptions.DisambiguationError:
+    ctypes.windll.user32.MessageBoxW(
+        0, "The word provided does not match any Wikipedia page!", "Error", 0
+    )
